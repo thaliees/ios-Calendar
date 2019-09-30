d6f2dd9063fc6c249b85909feedcc96072059aea
Thalia Aquino <heeytk@gmail.com>
2019-09-30 16:39:58 -0500

    Install Pod

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by SoftwareRB on 9/30/19.
+//  Copyright © 2019 Thaliees. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Calendar
 
 [Project created with Xcode V11] [Deployment Target = 13.0]
+
+## Implement CocoaPods and Pod JTAppleCalendar
+#### Install CocoaPods.
+$ sudo gem install cocoapods
+
+#### Create Podfile within of project
+$ pod init
+
+#### Modify Podfile
+pod 'JTAppleCalendar'
+
+#### Install Pod
+$ pod install
